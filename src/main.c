e3a2986faf082cb9d03f3aaeb027a76c559a3153
Ciremun <Ciremun@yandex.ru>
2021-02-19 12:56:42 +0300

    test b64

--- src/main.c
+++ src/main.c
@@ -8,17 +8,17 @@
 int main()
 {
     uint8_t key[] = "hgu575iue3jd8hjncnsgwfr2940";
-    uint8_t in[] = "PassWord123";
+    // uint8_t in[] = "owoimtryingalongpassWORDthistimeUwU";
     uint8_t iv[16] = {0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff};
 
-
     struct AES_ctx ctx;
-    AES_init_ctx_iv(&ctx, key, iv);
-    printf("ENC: %s\n", in);
-    AES_CTR_xcrypt_buffer(&ctx, in, strlen((char *)in));
-    printf("enc: %s\n", in);
+    // AES_init_ctx_iv(&ctx, key, iv);
+    // printf("ENC: %s\n", in);
+    // AES_CTR_xcrypt_buffer(&ctx, in, strlen((char *)in));
+    // printf("enc: %s\n", in);
 
-    char *be = b64_encode(in, strlen((char *)in));
+    char *be = "S9SNPDmB3lj58KRC6mckDSlKrpbGhixy7kuLZ69ZfuHQPfg=";
+    // char *be = b64_encode(mystr, strlen(mystr));
     printf("be: %s\n", be);
     char *bd = b64_decode(be, strlen(be));
     printf("bd: %s\n", bd);
@@ -33,6 +33,6 @@ int main()
     // fclose(f);
 
     AES_init_ctx_iv(&ctx, key, iv);
-    AES_CTR_xcrypt_buffer(&ctx, in, strlen((char *)in));
-    printf("DEC: %s\n", in);
+    AES_CTR_xcrypt_buffer(&ctx, bd, strlen(bd));
+    printf("DEC: %s\n", bd);
 }
