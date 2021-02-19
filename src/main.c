35ba3c9bdf21915893a58daf35811590f8dce8d1
Ciremun <Ciremun@yandex.ru>
2021-02-19 14:51:47 +0300

    write func

--- src/main.c
+++ src/main.c
@@ -6,13 +6,26 @@
 #include "aes.h"
 #include "b64/b64.h"
 
+struct AES_ctx ctx;
+
+void write(const char *filename, const char *mode, void *data)
+{
+    FILE *f = fopen(filename, mode);
+    if (f == NULL)
+    {
+        printf("Error opening file %s.\n", filename);
+        exit(1);
+    }
+    fprintf(f, "%s\n", data);
+    fclose(f);
+}
+
 int main()
 {
     uint8_t key[] = "7bf46a2a35655558232417df6887f9cfe57f32a4";
     uint8_t in[] = "kappa123";
     uint8_t iv[16] = {0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff};
 
-    struct AES_ctx ctx;
     AES_init_ctx_iv(&ctx, key, iv);
     printf("ENC: %s\n", in);
     AES_CTR_xcrypt_buffer(&ctx, in, strlen((char *)in));
@@ -21,17 +34,10 @@ int main()
     // char *be = "";
     char *be = b64_encode(in, strlen((char *)in));
     printf("be: %s\n", be);
+
+    write(".data", "a", be);
+
     char *bd = b64_decode(be, strlen(be));
-    FILE *f = fopen(".data", "w");
-    if (f == NULL)
-    {
-        printf("Error opening file!\n");
-        exit(1);
-    }
-
-    fprintf(f, "%s", be);
-    fclose(f);
-
     AES_init_ctx_iv(&ctx, key, iv);
     AES_CTR_xcrypt_buffer(&ctx, bd, strlen(bd));
     printf("DEC: %s\n", bd);
