33f5c0d6c41ebf20c1c99d55f457291650928e7b
Ciremun <Ciremun@yandex.ru>
2021-02-20 15:45:19 +0300

    update getline

--- src/main.c
+++ src/main.c
@@ -10,67 +10,52 @@
 #define LMAX 255
 
 
-ssize_t getline(char **lineptr, size_t *n, FILE *stream)
-{
-    char *bufptr = NULL;
-    char *p = bufptr;
-    size_t size;
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    size_t pos;
     int c;
 
-    if (lineptr == NULL)
-    {
+    if (lineptr == NULL || stream == NULL || n == NULL) {
+        errno = EINVAL;
         return -1;
     }
-    if (stream == NULL)
-    {
-        return -1;
-    }
-    if (n == NULL)
-    {
-        return -1;
-    }
-    bufptr = *lineptr;
-    size = *n;
 
-    c = fgetc(stream);
-    if (c == EOF)
-    {
+    c = getc(stream);
+    if (c == EOF) {
         return -1;
     }
-    if (bufptr == NULL)
-    {
-        bufptr = malloc(128);
-        if (bufptr == NULL)
-        {
+
+    if (*lineptr == NULL) {
+        *lineptr = malloc(128);
+        if (*lineptr == NULL) {
             return -1;
         }
-        size = 128;
+        *n = 128;
     }
-    p = bufptr;
-    while (c != EOF)
-    {
-        if ((size_t)(p - bufptr) > (size - 1))
-        {
-            size = size + 128;
-            bufptr = realloc(bufptr, size);
-            if (bufptr == NULL)
-            {
+
+    pos = 0;
+    while(c != EOF) {
+        if (pos + 1 >= *n) {
+            size_t new_size = *n + (*n >> 2);
+            if (new_size < 128) {
+                new_size = 128;
+            }
+            char *new_ptr = realloc(*lineptr, new_size);
+            if (new_ptr == NULL) {
                 return -1;
             }
+            *n = new_size;
+            *lineptr = new_ptr;
         }
-        *p++ = c;
-        if (c == '\n')
-        {
+
+        ((unsigned char *)(*lineptr))[pos ++] = c;
+        if (c == '\n') {
             break;
         }
-        c = fgetc(stream);
+        c = getc(stream);
     }
 
-    *p++ = '\0';
-    *lineptr = bufptr;
-    *n = size;
-
-    return p - bufptr - 1;
+    (*lineptr)[pos] = '\0';
+    return pos;
 }
 
 char** read_file(const char *fp, size_t *lsize)
