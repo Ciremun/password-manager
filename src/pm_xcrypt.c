3d567c353726c8ea5b633bbfaa5329f6635c40be
Ciremun <Ciremun@yandex.ru>
2021-12-04 13:25:57 +0300

    fmt

--- src/pm_xcrypt.c
+++ src/pm_xcrypt.c
@@ -284,14 +284,14 @@ void decrypt_and_print(Flags *fl, uint8_t *aes_key)
             size_t b64_decoded_len;
             uint8_t *b64_decoded_str = b64_decode_ex(f.start + i, p - i, &b64_decoded_len);
             xcrypt_buffer(b64_decoded_str, aes_key, b64_decoded_len);
-	    if (fl->find_label.exists &&
-                (label_len+1 >= b64_decoded_len || (memcmp(b64_decoded_str, fl->find_label.value, label_len) != 0)))
+            if (fl->find_label.exists &&
+                (label_len + 1 >= b64_decoded_len || (memcmp(b64_decoded_str, fl->find_label.value, label_len) != 0)))
                 goto skip_write;
             if (fwrite(b64_decoded_str, 1, b64_decoded_len, o) != b64_decoded_len)
                 error("%s", "fwrite failed");
             fputc('\n', o);
-skip_write:
-	    i = p + 1;
+        skip_write:
+            i = p + 1;
             free(b64_decoded_str);
         }
         p++;
