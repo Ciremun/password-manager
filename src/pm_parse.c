09a0d426b0a43c9b59aa64531c5db6c5bda8f6cd
Ciremun <Ciremun@yandex.ru>
2021-12-10 00:20:23 +0300

    pvs-studio comment

--- src/pm_parse.c
+++ src/pm_parse.c
@@ -1,3 +1,6 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "pm_parse.h"
 #include "pm_io.h"
 #include "pm_xcrypt.h"
