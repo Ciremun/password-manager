40359e15e4f8e6565f54cdf707c0c98430a1446b
Ciremun <Ciremun@yandex.ru>
2021-12-10 00:20:23 +0300

    pvs-studio comment

--- src/core/pm_sync.c
+++ src/core/pm_sync.c
@@ -1,3 +1,6 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 // Copyright 2021 Alexey Kutepov <reximkut@gmail.com>
 
 // Permission is hereby granted, free of charge, to any person obtaining
