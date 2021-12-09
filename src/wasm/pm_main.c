4be88c650fad029c2184a6f8da370744f455ac74
Ciremun <Ciremun@yandex.ru>
2021-12-09 18:52:25 +0300

    text input init

--- src/wasm/pm_main.c
+++ src/wasm/pm_main.c
@@ -23,6 +23,14 @@
 #define WINDOW_NAME "password-manager"
 
 short w, h;
+int offset = 0;
+char str[64] = {0};
+
+void EXPORT("HandleKey") HandleKey(int keycode, int bDown)
+{
+    if (bDown)
+        str[offset++] = keycode;
+}
 
 void setup_window()
 {
@@ -39,12 +47,39 @@ void setup_window()
 #endif // __ANDROID__
 }
 
+extern unsigned char __heap_base;
+char *heap = (char *)&__heap_base;
+
+unsigned long strlen(const char *s)
+{
+    unsigned long sz = 0;
+    while (s[sz] != '\0')
+        sz++;
+    return sz;
+}
+
+void *memset(void *dest, int val, unsigned long len)
+{
+    unsigned char *ptr = dest;
+    while (len-- > 0)
+        *ptr++ = val;
+    return dest;
+}
+
+void *memcpy(void *dst, void const *src, unsigned long size)
+{
+    unsigned char *source = (unsigned char *)src;
+    unsigned char *dest = (unsigned char *)dst;
+    while (size--)
+        *dest++ = *source++;
+    return dst;
+}
+
 int EXPORT("main") main()
 {
     CNFGBGColor = BLACK;
     setup_window();
 
-    char str[64] = {0};
 #ifdef RAWDRAW_USE_LOOP_FUNCTION
     return 0;
 }
@@ -63,8 +98,13 @@ int EXPORT("loop") loop()
 #endif // __wasm__
 
         CNFGColor(COLOR(0xffffffff));
-        CNFGTackRectangle(50, 50, w / 2 - 100, 100);
+        CNFGTackRectangle(w / 10, h / 2 + 25, w - w / 10, h / 2 - 25);
 
+        CNFGPenX = w / 10 + 10;
+        CNFGPenY = h / 2 - 10;
+        CNFGColor(BLACK);
+        CNFGDrawText(str, 5);
+        
         CNFGSwapBuffers();
     }
 
