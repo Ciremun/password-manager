ba8d3f08c52e53348bb893ad43ef3a4aa4d0627c
Ciremun <Ciremun@yandex.ru>
2022-01-03 17:55:20 +0300

    pvs studio comment

--- source/rawdraw/rd_xcrypt.c
+++ source/rawdraw/rd_xcrypt.c
@@ -1,3 +1,6 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "rawdraw/vendor/os_generic.h"
 
 #include "rawdraw/rd_xcrypt.h"
--- source/wasm/wasm_xcrypt.c
+++ source/wasm/wasm_xcrypt.c
@@ -1,3 +1,6 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "rawdraw/rd_util.h"
 #include "rawdraw/rd_ui.h"
 #include "wasm/wasm_xcrypt.h"
