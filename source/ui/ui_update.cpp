83c527b83adfa1214c52b0434c4ad8daa1a65fe5
Ciremun <Ciremun@yandex.ru>
2022-01-28 02:31:08 +0300

    remove demo, define imvec math ops, enter password

--- source/ui/ui_update.cpp
+++ source/ui/ui_update.cpp
@@ -1,8 +1,35 @@
-#include "ui/update.hpp"
+#include <stdint.h>
+#include <stdio.h>
+
 #include "stb_sprintf.h"
+#include "imgui_internal.h"
+
+#include "ui/update.hpp"
+
+static char password[32] = {0};
+static bool password_entered = false;
 
 void ui_update()
 {
+    if (!password_entered)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        ImGui::SetNextWindowPos(ImVec2(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+        ImGui::SetNextWindowSize(ImVec2(400.0f, 70.0f), ImGuiCond_Always);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("Enter password", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+            ImGui::PushItemWidth(ImGui::GetWindowWidth());
+            if (ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows) && !ImGui::IsAnyItemActive() && !ImGui::IsMouseClicked(0))
+                ImGui::SetKeyboardFocusHere(0);
+            ImGui::SetCursorPos(ImVec2(0.0f, ImGui::GetWindowHeight() * 0.5f));
+            if (ImGui::InputText("##password", password, 32, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_Password) && password[0])
+                password_entered = true;
+            ImGui::PopItemWidth();
+        ImGui::End();
+        ImGui::PopStyleVar();
+        if (!password_entered)
+            return;
+    }
     ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f), ImGuiCond_Once);
     ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
     ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
