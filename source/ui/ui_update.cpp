c865a49cecf711adfa219092960678389dda1e50
Ciremun <Ciremun@yandex.ru>
2022-01-28 23:17:36 +0300

    save changes

--- source/ui/ui_update.cpp
+++ source/ui/ui_update.cpp
@@ -4,12 +4,17 @@
 #include "imgui_internal.h"
 
 #include "console/util.h"
+#include "console/io.h"
 #include "ui/update.hpp"
 #include "ui/xcrypt.hpp"
 
 static uint8_t aes_key[32] = {0};
 static bool password_entered = false;
-// static ImVector<String> passwords;
+static ImVector<String> passwords;
+
+String sync_remote_url;
+
+extern const char* data_store;
 
 void ui_update()
 {
@@ -19,8 +24,7 @@ void ui_update()
         ImGui::SetNextWindowPos(ImVec2(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
         ImGui::SetNextWindowSize(ImVec2(400.0f, 70.0f), ImGuiCond_Always);
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-        ImGui::Begin("Enter aes_key", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
-        {
+        ImGui::Begin("Enter password", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
             ImGui::PushItemWidth(ImGui::GetWindowWidth());
             if (ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows) && !ImGui::IsAnyItemActive() && !ImGui::IsMouseClicked(0))
                 ImGui::SetKeyboardFocusHere(0);
@@ -28,27 +32,25 @@ void ui_update()
             if (ImGui::InputText("##aes_key", (char *)aes_key, 32, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_Password) && aes_key[0])
                 password_entered = true;
             ImGui::PopItemWidth();
-        }
         ImGui::End();
         ImGui::PopStyleVar();
         if (!password_entered)
             return;
-        // passwords.reserve(256);
-        // ui_load_passwords(aes_key, passwords);
+        data_store = DEFAULT_DATA_STORE;
+        passwords.reserve(256);
+        ui_load_passwords(aes_key, passwords);
     }
     ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f), ImGuiCond_Once);
     ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
     ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
     ImGui::Begin("##io", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
-    {
-        // for (int i = 0; i < passwords.size(); ++i)
-        // {
-        //     const auto &password = passwords[i];
-        //     ImGui::PushID(i);
-        //     ImGui::InputText("", (char *)password.data, password.length);
-        //     ImGui::PopID();
-        // }
-    }
+        for (int i = 0; i < passwords.size(); ++i)
+        {
+            const auto &password = passwords[i];
+            ImGui::PushID(i);
+            ImGui::InputText("", (char *)password.data, password.length);
+            ImGui::PopID();
+        }
     ImGui::End();
     ImGui::PopStyleVar();
 }
