5eaf74e32e1c2b847ac35e2c26762491a0fb2a87
Ciremun <Ciremun@yandex.ru>
2022-03-20 20:59:38 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cc -DNDEBUG -O3 -g0 source/console/*.c -Isource/include -opm
+cc -DNDEBUG -DPM_VERSION=$(git rev-parse --short HEAD) -O3 -g0 source/console/*.c -Iinclude -opm-$(uname -s -m | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
