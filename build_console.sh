8ff640a788d4904254ab742d36f8dbe0f15fd64e
Ciremun <Ciremun@yandex.ru>
2022-03-28 02:46:10 +0300

    mark as executable

--- build_console.sh
+++ build_console.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 cc -DNDEBUG -DPM_VERSION=$(git rev-parse --short HEAD) -O3 -g0 source/console/pm_main.c source/console/pm_aes.c source/console/pm_b64.c source/console/pm_io.c source/console/pm_parse.c source/console/pm_rand.c source/console/pm_sync.c source/console/pm_xcrypt.c -Iinclude -opm-$(uname -s -m | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
