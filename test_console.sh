6f6603d03e8dbe638d4e3cd14061a74248a515d2
Ciremun <Ciremun@yandex.ru>
2022-04-01 03:31:45 +0300

    -lpthread

--- test_console.sh
+++ test_console.sh
@@ -1,1 +1,1 @@
-cc -DTEST source/console/test.c source/console/pm_thread.c source/console/pm_aes.c source/console/pm_b64.c source/console/pm_io.c source/console/pm_parse.c source/console/pm_rand.c source/console/pm_sync.c source/console/pm_xcrypt.c -ggdb -O0 -Iinclude -otest_console && ./test_console
+cc -DTEST source/console/test.c source/console/pm_thread.c source/console/pm_aes.c source/console/pm_b64.c source/console/pm_io.c source/console/pm_parse.c source/console/pm_rand.c source/console/pm_sync.c source/console/pm_xcrypt.c -ggdb -O0 -Iinclude -otest_console -lpthread && ./test_console
